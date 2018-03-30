d9c4bfbc5490421ed148e1da18fab184f27bc238
EdAlegrid <ealegrid17@gmail.com>
2018-03-30 05:09:40 -0500

    edit license info

--- rpi.c
+++ rpi.c
@@ -1,20 +1,7 @@
 /*
- * This is a low-level Peripherals Control Library for ARM-based SBC's such as Raspberry Pi 1 Model B+, 2, 3 and Pi Zero W.
- *
+ * rpi.c
  * Copyright (c) 2017 Ed Alegrid <ealegrid@gmail.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
+ * GNU General Public License v3.0
  */
 
 #define  _DEFAULT_SOURCE	// for nanosleep() and usleep()
