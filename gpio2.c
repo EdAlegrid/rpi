ffb171535bf757c4993183d04e84d3826943e0dd
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-07-21 07:10:17 -0400

    Update gpio2.c

--- gpio2.c
+++ gpio2.c
@@ -16,10 +16,9 @@
  *
  * Reboot your system.
  *
- * This will disable GPIO interrupts and avoids crashes with newer kernels when using edge interrupts.
+ * This will disable GPIO interrupts avoiding system crashes with newer kernels when using edge interrupts.
  * 
  */
-
 
 
 #include <stdio.h>
@@ -47,8 +46,8 @@
  */
 
 /* input pins */
-uint8_t inputPin = 17;  //switch to turn ON  LED output
-uint8_t inputPin2 = 27; //switch to turn OFF LED output
+uint8_t inputPin1 = 17;  //switch to turn ON  LED output
+uint8_t inputPin2 = 27;  //switch to turn OFF LED output
 
 /* output pins */
 uint8_t outputPin = 13; //LED output
@@ -57,16 +56,16 @@ uint8_t statusPin = 20; //LED status indicator
 /* Ctrl-C handler */
 void sighandler(int signum)
 {
-   	printf("\nsighandler invoked %d, exiting ...\n", signum);
-		gpio_reset_all_events(inputPin);
-    gpio_reset_all_events(inputPin2);
+	printf("\nsighandler invoked %d, exiting ...\n", signum);
+	gpio_reset_all_events(inputPin1);
+    	gpio_reset_all_events(inputPin2);
 
-    gpio_write(statusPin, 0);  //turn OFF
-    gpio_config(statusPin, 0); //set to GPIO input
-		gpio_write(outputPin, 0);  //turn OFF
-    gpio_config(outputPin, 0); //set to GPIO input
+    	gpio_write(statusPin, 0);  //turn OFF
+    	gpio_config(statusPin, 0); //set to GPIO input
+	gpio_write(outputPin, 0);  //turn OFF
+    	gpio_config(outputPin, 0); //set to GPIO input
 
-    puts("closing rpi ...");
+    	puts("Closing rpi ...");
    	rpi_close();
    	exit(1);
 }
@@ -75,12 +74,11 @@ void sighandler(int signum)
     main
 *************/
 int main(void){
+	signal(SIGINT, sighandler);
 
-		signal(SIGINT, sighandler);
-
-  	puts("starting rpi ...");
-		rpi_init(0);
-    /* set statusPin to GPIO output */
+  	puts("Initializing rpi ...");
+	rpi_init(0);
+    	/* set statusPin to GPIO output */
   	gpio_config(statusPin, 1);
 
   	/* set outputPin to GPIO output */
@@ -88,36 +86,36 @@ int main(void){
   	/* set outputPin initial state to OFF */
   	gpio_write(outputPin, 0);
 
-    /* set inputPin and inputPin2 to GPIO inputs */
-		gpio_config(inputPin, 0);
-    gpio_config(inputPin2, 0);
+    	/* set inputPin and inputPin2 to GPIO inputs */
+	gpio_config(inputPin1, 0);
+    	gpio_config(inputPin2, 0);
 
   	/* reset any previous input events setup */
-  	gpio_reset_all_events(inputPin);
+  	gpio_reset_all_events(inputPin1);
   	gpio_reset_all_events(inputPin2);
 
   	/* enable async_rising edge detection event */
-  	gpio_enable_async_rising_event(inputPin, 1);
+  	gpio_enable_async_rising_event(inputPin1, 1);
   	gpio_enable_async_rising_event(inputPin2, 1);
 
-  	puts("starting GPIO Input Pin Event Detection loop");
+  	puts("Starting GPIO Input Pin Event Detection loop ...");
   	while(1) {
     		gpio_write(statusPin, 1);
-        /* press inputPin switch to turn ON LED output */
-    		if(gpio_detect_input_event(inputPin)){
-				puts("turning ON  outputPin ...");
+        	/* press inputPin switch to turn ON LED output */
+    		if(gpio_detect_input_event(inputPin1)){
+			puts("turning ON  outputPin ...");
       			gpio_write(outputPin, 1);
-				gpio_reset_event(inputPin);
+			gpio_reset_event(inputPin1);
     		}
 
-				/* press inputPin2 switch to turn OFF LED output */
+		/* press inputPin2 switch to turn OFF LED output */
     		if(gpio_detect_input_event(inputPin2)){
- 				puts("turning OFF outputPin ...");
+ 			puts("turning OFF outputPin ...");
       			gpio_write(outputPin, 0);
-				gpio_reset_event(inputPin2);
+			gpio_reset_event(inputPin2);
     		}
 
-        /* status LED will blink, indicates loop is running */
+        	/* status LED will continously blink indicating the event loop is running */
     		mswait(300);
     		gpio_write(statusPin, 0);
     		mswait(200);
